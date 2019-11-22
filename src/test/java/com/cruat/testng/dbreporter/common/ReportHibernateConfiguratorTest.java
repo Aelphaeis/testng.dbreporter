add28a0ed65dd553f3042adc4ba1b74d79a83dda
Joseph Morain <morain@echoworx.com>
2019-11-22 15:54:41 -0500

    unit tests for report hibernate configurator

--- src/test/java/com/cruat/testng/dbreporter/common/ReportHibernateConfiguratorTest.java
+++ src/test/java/com/cruat/testng/dbreporter/common/ReportHibernateConfiguratorTest.java
@@ -2,9 +2,26 @@ package com.cruat.testng.dbreporter.common;
 
 import static org.junit.Assert.assertNotNull;
 
+import javax.persistence.EntityManagerFactory;
+
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 public class ReportHibernateConfiguratorTest{
+	
+	private static final String CONN_STR_TEMPLATE = "jdbc:sqlite:%s/test.db";
+	private String connString;
+	
+	@Rule
+	public TemporaryFolder folder = new TemporaryFolder();
+	
+	@Before
+	public void setup() {
+		String location = folder.getRoot().toString().replace("\\", "/");
+		connString = String.format(CONN_STR_TEMPLATE, location);
+	}
 	
 	@Test(expected = NullPointerException.class)
 	public void ctor_null_npe() {
@@ -14,5 +31,12 @@ public class ReportHibernateConfiguratorTest{
 	@Test
 	public void ctor_url_instance() {
 		assertNotNull(new ReportHibernateConfigurator(""));
+	}
+	
+	@Test
+	public void buildFactory_validConnectionString_correctlyBound() {
+		EntityManagerFactory f = new ReportHibernateConfigurator(connString)
+				.buildFactory();
+		assertNotNull(f);
 	}
 }
