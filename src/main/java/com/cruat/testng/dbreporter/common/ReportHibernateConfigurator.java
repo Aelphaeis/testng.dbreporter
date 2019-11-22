06da1c520d4ae86d9fbdec113c574ea821294a7f
Joseph Morain <morain@echoworx.com>
2019-11-22 15:50:25 -0500

    added automatic resolution of dialect resolution

--- src/main/java/com/cruat/testng/dbreporter/common/ReportHibernateConfigurator.java
+++ src/main/java/com/cruat/testng/dbreporter/common/ReportHibernateConfigurator.java
@@ -6,16 +6,16 @@ import java.util.Objects;
 
 import javax.persistence.EntityManagerFactory;
 
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.dialect.MySQL8Dialect;
 
 import com.cruat.testng.dbreporter.entities.ReportEntity;
 import com.cruat.testng.dbreporter.entities.TestNGResults;
 
 class ReportHibernateConfigurator{
 	
-	private static final String DIALECT = getDialect();
 	private final String url;
 	
 	public ReportHibernateConfigurator(String url) {
@@ -23,16 +23,20 @@ class ReportHibernateConfigurator{
 	}
 	
 	public EntityManagerFactory buildFactory() {
-		Configuration conf = new Configuration();
+		Configuration conf = new Configuration()
+				.setProperty(AvailableSettings.AUTOCOMMIT, "true")
+				.setProperty(AvailableSettings.URL, url);
+		
 		for (Class<? extends ReportEntity> cls : getAnnotated()) {
 			conf.addAnnotatedClass(cls);
 		}
 		
-		return conf
-				.setProperty(AvailableSettings.DIALECT, DIALECT)
-				.setProperty(AvailableSettings.AUTOCOMMIT, "true")
-				.setProperty(AvailableSettings.URL, url)
-				.buildSessionFactory();
+		StandardServiceRegistry reg = new StandardServiceRegistryBuilder()
+				.applySettings(conf.getProperties())
+				.build();
+		
+		return conf.buildSessionFactory(reg);
+		
 	}
 	
 	public static List<Class<? extends ReportEntity>> getAnnotated() {
@@ -41,7 +45,4 @@ class ReportHibernateConfigurator{
 		return annotated;
 	}
 	
-	private static String getDialect() {
-		return MySQL8Dialect.class.getName();
-	}
 }
