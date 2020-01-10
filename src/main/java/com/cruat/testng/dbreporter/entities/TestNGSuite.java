963cc3652b348315f1744f8ecadc7e8e355dac57
Joseph Morain <morain@echoworx.com>
2020-01-10 17:38:35 -0500

    testngsuite

---
+++ src/main/java/com/cruat/testng/dbreporter/entities/TestNGSuite.java
@@ -0,0 +1,96 @@
+package com.cruat.testng.dbreporter.entities;
+
+import java.time.OffsetDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "testng_suites")
+public class TestNGSuite {
+
+	private long id;
+	private String name;
+	private OffsetDateTime startDatetime;
+	private OffsetDateTime endDateTime;
+	private long resultId;
+	
+	/**
+	 * @return the id
+	 */
+	@Id
+	@Column(name = "id")
+	public long getId() {
+		return id;
+	}
+	
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	/**
+	 * @return the name
+	 */
+	@Column(name = "name")
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * @return the startDatetime
+	 */
+	@Column(name = "start")
+	public OffsetDateTime getStartDatetime() {
+		return startDatetime;
+	}
+	
+	/**
+	 * @param startDatetime the startDatetime to set
+	 */
+	public void setStartDatetime(OffsetDateTime startDatetime) {
+		this.startDatetime = startDatetime;
+	}
+	
+	/**
+	 * @return the endDateTime
+	 */
+	@Column(name = "end")
+	public OffsetDateTime getEndDateTime() {
+		return endDateTime;
+	}
+	
+	/**
+	 * @param endDateTime the endDateTime to set
+	 */
+	public void setEndDateTime(OffsetDateTime endDateTime) {
+		this.endDateTime = endDateTime;
+	}
+	
+	/**
+	 * @return the resultId
+	 */
+	public long getResultId() {
+		return resultId;
+	}
+	
+	/**
+	 * @param resultId the resultId to set
+	 */
+	public void setResultId(long resultId) {
+		this.resultId = resultId;
+	};
+	
+	
+}
