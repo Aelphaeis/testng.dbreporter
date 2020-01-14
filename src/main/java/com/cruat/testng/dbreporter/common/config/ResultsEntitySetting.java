1f8ef3b4c631bbdea2db218892fdf92ef7cce452
Joseph <aelphaeis@live.com>
2020-01-14 13:40:18 -0500

    added resultsEntitySetting

---
+++ src/main/java/com/cruat/testng/dbreporter/common/config/ResultsEntitySetting.java
@@ -0,0 +1,16 @@
+package com.cruat.testng.dbreporter.common.config;
+
+public class ResultsEntitySetting implements Setting {
+	
+
+	@Override
+	public String name() {
+		return "reporting.entities.testng_results";
+	}
+
+	@Override
+	public String value() {
+		return null;
+	}
+
+}
