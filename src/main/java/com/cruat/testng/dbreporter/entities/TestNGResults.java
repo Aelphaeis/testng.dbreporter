daf8148768e1860d2b38cdd49ce7eb04c99b187c
Joseph <aelphaeis@live.com>
2020-01-14 19:27:02 -0500

    explicitly specify generation value.

--- src/main/java/com/cruat/testng/dbreporter/entities/TestNGResults.java
+++ src/main/java/com/cruat/testng/dbreporter/entities/TestNGResults.java
@@ -4,6 +4,8 @@ import java.time.OffsetDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -24,6 +26,7 @@ public class TestNGResults implements ReportEntity {
 	 */
 	@Id
 	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public long getId() {
 		return id;
 	}
