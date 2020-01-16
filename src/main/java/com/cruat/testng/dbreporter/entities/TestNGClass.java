5ffe2fbc64289ee28bedbb1c8161ce507cabdbbf
Joseph Morain <morain@echoworx.com>
2020-01-16 18:30:08 -0500

    clean up testng class

--- src/main/java/com/cruat/testng/dbreporter/entities/TestNGClass.java
+++ src/main/java/com/cruat/testng/dbreporter/entities/TestNGClass.java
@@ -1,7 +1,42 @@
 package com.cruat.testng.dbreporter.entities;
 
-
 public class TestNGClass {
+	
 	private String name;
 	private String pkgName;
+	
+	public TestNGClass() {
+		throw new UnsupportedOperationException("Not Implemented");
+	}
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * @return the pkgName
+	 */
+	public String getPkgName() {
+		return pkgName;
+	}
+	
+	/**
+	 * @param pkgName
+	 *            the pkgName to set
+	 */
+	public void setPkgName(String pkgName) {
+		this.pkgName = pkgName;
+	}
+	
 }
