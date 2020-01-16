ed2334f3899e14bf5fb41af54e916afb1290172b
Joseph Morain <morain@echoworx.com>
2020-01-16 18:38:23 -0500

    additonal fields for TNGM

--- src/main/java/com/cruat/testng/dbreporter/entities/TestNGMethod.java
+++ src/main/java/com/cruat/testng/dbreporter/entities/TestNGMethod.java
@@ -1,11 +1,19 @@
 package com.cruat.testng.dbreporter.entities;
 
+import java.time.OffsetDateTime;
+
 import org.testng.ITestResult;
 
 public class TestNGMethod {
 	
 	private String status;
 	private boolean retried;
+	private boolean isConfig;
+	private String name;
+	private String description;
+	private String signature;
+	private OffsetDateTime start;
+	private OffsetDateTime end;
 	
 	public TestNGMethod() {
 		this(null);
@@ -43,5 +51,95 @@ public class TestNGMethod {
 	 */
 	public void setRetried(boolean retried) {
 		this.retried = retried;
+	}
+	
+	/**
+	 * @return the isConfig
+	 */
+	public boolean isConfig() {
+		return isConfig;
+	}
+	
+	/**
+	 * @param isConfig
+	 *            the isConfig to set
+	 */
+	public void setConfig(boolean isConfig) {
+		this.isConfig = isConfig;
+	}
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	
+	/**
+	 * @param description
+	 *            the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	/**
+	 * @return the signature
+	 */
+	public String getSignature() {
+		return signature;
+	}
+	
+	/**
+	 * @param signature
+	 *            the signature to set
+	 */
+	public void setSignature(String signature) {
+		this.signature = signature;
+	}
+	
+	/**
+	 * @return the start
+	 */
+	public OffsetDateTime getStart() {
+		return start;
+	}
+	
+	/**
+	 * @param start
+	 *            the start to set
+	 */
+	public void setStart(OffsetDateTime start) {
+		this.start = start;
+	}
+	
+	/**
+	 * @return the end
+	 */
+	public OffsetDateTime getEnd() {
+		return end;
+	}
+	
+	/**
+	 * @param end
+	 *            the end to set
+	 */
+	public void setEnd(OffsetDateTime end) {
+		this.end = end;
 	}
 }
