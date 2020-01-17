2aa0e47ba0ef2951cc24f9672ca49cc2af1d6607
Joseph Morain <morain@echoworx.com>
2020-01-17 13:09:52 -0500

    ITRS

---
+++ src/main/java/com/cruat/testng/dbreporter/entities/ITestResultStatus.java
@@ -0,0 +1,31 @@
+package com.cruat.testng.dbreporter.entities;
+
+import java.util.Arrays;
+
+public enum ITestResultStatus {
+	
+	SUCCESS(1),
+	FAILURE(2),
+	SKIP(3),
+	SUCCESS_PERCENTAGE_FAILURE(4);
+	
+	private final int value;
+	
+	private ITestResultStatus(int value) {
+		this.value = value;
+	}
+	
+	/**
+	 * @return the value
+	 */
+	public int getValue() {
+		return value;
+	}
+	
+	public static ITestResultStatus valueOf(int i) {
+		return Arrays.stream(ITestResultStatus.values())
+				.filter(p -> p.getValue() == i)
+				.findAny()
+				.orElseThrow(IllegalArgumentException::new);
+	}
+}
